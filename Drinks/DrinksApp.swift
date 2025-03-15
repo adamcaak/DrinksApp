b28e2c9c8932cdb7644793f5b091e969460a6a32
adamcaak <ladamczak1997@gmail.com>
2025-03-15 20:38:52 +0100

    Initial Commit

---
+++ Drinks/DrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinksApp.swift
+//  Drinks
+//
+//  Created by Łukasz Adamczak on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
